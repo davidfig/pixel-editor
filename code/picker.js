23ecf7dd2f1fbdbcd5a741754b097b4633072ca3
David Figatner <david@yopeyopey.com>
2018-01-04 11:03:58 +0800

    alpha bar looks nice

--- code/picker.js
+++ code/picker.js
@@ -11,8 +11,11 @@ const MIN_HEIGHT = 300
 const CONTROL = 5
 const WIDTH = 4
 
+const LINE_WIDTH = 1
+const BAR_WIDTH = LINE_WIDTH * 6
 const BAR_HEIGHT = 25
-const RADIUS = 5
+const RADIUS = 6
+const SPACING = 8
 
 module.exports = class Picker
 {
@@ -25,7 +28,7 @@ module.exports = class Picker
         this.content = this.win.$content[0]
         this.content.style.margin = '0.5em'
 
-        this.renderer = new PIXI.WebGLRenderer({ resolution: window.devicePixelRatio, transparent: true })
+        this.renderer = new PIXI.WebGLRenderer({ resolution: window.devicePixelRatio, transparent: true, preserveDrawingBuffer: true })
 
         this.stateSetup('picker')
         this.content.appendChild(this.renderer.view)
@@ -33,9 +36,11 @@ module.exports = class Picker
         this.stage = new PIXI.Container()
 
         this.sheet = new RenderSheet()
-        this.sheet.add('picker', (c) => this.drawPicker(c), () => this.measurePicker())
-        this.sheet.add('pickerCursor', (c) => this.drawPickerCursor(c), () => { return { width: RADIUS * 2 + 1, height: RADIUS * 2 + 1 } })
-        // this.sheet.add('hue', (c) => this.drawHue(c), (c) => this.measureBar(c))
+        this.sheet.add('picker', (c) => this.drawPicker(c), () => { return { width: this.size(), height: this.size() } })
+        this.sheet.add('pickerCursor', (c) => this.drawPickerCursor(c), () => { return { width: RADIUS * 2 + LINE_WIDTH / 2, height: RADIUS * 2 + LINE_WIDTH / 2 } })
+        this.sheet.add('bar', (c) => this.drawBar(c), () => { return { width: BAR_WIDTH, height: BAR_HEIGHT + LINE_WIDTH * 4 }})
+        this.sheet.add('hue', (c) => this.drawHue(c), () => { return { width: this.size(), height: 1 } })
+        this.sheet.add('alpha', (c) => this.drawAlpha(c), () => { return { width: this.size(), height: BAR_HEIGHT } })
 
         // this.sheet.show = true
 
@@ -45,8 +50,8 @@ module.exports = class Picker
     afterRender()
     {
         this.setupPicker()
-        // this.setupHue()
-        // this.setupAlpha()
+        this.setupHue()
+        this.setupAlpha()
 
         this.renderer.render(this.stage)
 
@@ -57,36 +62,35 @@ module.exports = class Picker
         this.wordsSetup()
     }
 
+    size()
+    {
+        return this.content.offsetWidth - RADIUS * 2
+    }
+
     setupPicker()
     {
         this.picker = this.stage.addChild(new PIXI.Container())
+        this.picker.position.set(RADIUS)
         const gradient = this.picker.addChild(this.sheet.get('picker'))
         gradient.anchor.set(0)
-        gradient.position.set(RADIUS)
         this.picker.interactive = true
         this.picker.on('pointerdown', () => this.isPickerDown = true)
-        // this.picker.on('pointermove', (e) => this.pickerMove(e))
+        this.picker.on('pointermove', (e) => this.pickerMove(e))
         this.picker.on('pointerup', () => this.isPickerDown = false)
-
+        this.picker.on('pointerupoutside', () => this.isPickerDown = false)
         this.pickerCursor = this.picker.addChild(this.sheet.get('pickerCursor'))
-
-        this.ui.$overlay[0].addEventListener('mousemove', (e) => this.pickerMove(e))
-        this.win.el[0].addEventListener('mousemove', (e) => this.pickerMove(e))
-        this.ui.$overlay[0].addEventListener('mouseup', () => this.pickerUp())
-        this.win.el[0].addEventListener('mouseup', () => this.pickerUp())
-    }
-
-    measurePicker()
-    {
-        return { width: this.content.offsetWidth - RADIUS * 2, height: this.content.offsetWidth - RADIUS * 2 }
+        const size = this.size()
+        const color = new TinyColor('#' + State.color.substr(0, 6)).toHsl()
+        this.pickerCursor.position.set(size * color.s, size * (1 - color.l))
     }
 
     drawPicker(c)
     {
+        c.beginPath()
         const color = '#' + State.color.substr(0, 6)
         const translate = new TinyColor(color).toHsl()
         const show = new TinyColor({ h: translate.h, s: 1, l: 0.5 })
-        const size = this.measurePicker().width
+        const size = this.size()
         c.fillStyle = show
         c.fillRect(0, 0, size, size)
         let gradient = c.createLinearGradient(0, 0, size, 0)
@@ -105,13 +109,14 @@ module.exports = class Picker
 
     drawPickerCursor(c)
     {
-        const middle = RADIUS + 0.5
-        c.lineWidth = '1px'
+        c.beginPath()
+        c.lineWidth = LINE_WIDTH / 2
+        const middle = RADIUS + LINE_WIDTH / 4
         c.strokeStyle = 'black'
-        c.arc(middle, middle.y, RADIUS, 0, 2 * Math.PI)
+        c.arc(middle, middle, RADIUS - LINE_WIDTH / 2, 0, 2 * Math.PI)
         c.stroke()
         c.strokeStyle = 'white'
-        c.arc(middle, middle, RADIUS - 1, 0, 2 * Math.PI)
+        c.arc(middle, middle, RADIUS - 1 * LINE_WIDTH, 0, 2 * Math.PI)
         c.stroke()
     }
 
@@ -119,15 +124,21 @@ module.exports = class Picker
     {
         if (this.isPickerDown)
         {
-            const size = this.content.offsetWidth - RADIUS * 2
-            const local = this.win.toLocal({ x: e.offsetX, y: e.offsetY })
-            let x = local.x - this.picker.x
-            x = x < RADIUS ? RADIUS : x > size + RADIUS ? size + RADIUS : x
-            let y = local.y - this.picker.y
-            y = y < RADIUS ? RADIUS : y > size + RADIUS ? size + RADIUS : y
+            const size = this.size()
+            const local = this.picker.toLocal(e.data.global)
+            let x = local.x
+            x = x < 0 ? 0 : x > size ? size : x
+            let y = local.y
+            y = y < 0 ? 0 : y > size ? size : y
             this.pickerCursor.position.set(x, y)
             this.renderer.render(this.stage)
-console.log(x, y)
+
+            const s = x / size
+            const l = 1 - y / size
+            const hue = new TinyColor('#' + State.color.substr(0, 6)).toHsl()
+            const color = new TinyColor({ h: hue.h, s, l }).toHex()
+            State.color = color + State.color.substr(6)
+            console.log(color, State.color)
         }
     }
 
@@ -136,28 +147,19 @@ console.log(x, y)
         this.isPickerDown = false
     }
 
-    setupHue()
+    drawBar(c)
     {
-        this.hue = document.createElement('canvas')
-        this.hue.style.width = '100%'
-        // this.hue.addEventListener('mousedown', () => this.pickerDown())
-        // this.hue.addEventListener('mousemove', (e) => this.hueMove(e))
-        // this.ui.$overlay[0].addEventListener('mousemove', (e) => this.hueMove(e))
-        // this.win.el[0].addEventListener('mousemove', (e) => this.hueMove(e))
-        // this.hue.addEventListener('mouseup', () => this.hueUp())
-        // this.ui.$overlay[0].addEventListener('mouseup', () => this.hueUp())
-        // this.win.el[0].addEventListener('mouseup', () => this.hueUp())
-        this.content.appendChild(this.hue)
-        this.drawHue()
+        c.beginPath()
+        c.lineWidth = LINE_WIDTH * 2
+        c.strokeStyle = 'black'
+        c.rect(LINE_WIDTH, LINE_WIDTH, BAR_WIDTH - RADIUS / 2, BAR_HEIGHT + LINE_WIDTH * 2)
+        c.stroke()
     }
 
-    drawHue()
+    drawHue(c)
     {
-        // const color = '#' + State.color.substr(0, 6)
-        this.hue.width = this.content.offsetWidth
-        this.hue.height = BAR_HEIGHT
-        const c = this.hue.getContext('2d')
-        let gradient = c.createLinearGradient(0, 0, this.size, 0)
+        c.beginPath()
+        let gradient = c.createLinearGradient(0, 0, this.size(), 0)
         gradient.addColorStop(0, '#f00')
         gradient.addColorStop(0.1666, '#ff0')
         gradient.addColorStop(0.3333, '#0f0')
@@ -166,35 +168,93 @@ console.log(x, y)
         gradient.addColorStop(0.8333, '#f0f')
         gradient.addColorStop(1, '#f00')
         c.fillStyle = gradient
-        c.fillRect(RADIUS, RADIUS, this.size, BAR_HEIGHT)
+        c.fillRect(0, 0, this.size(), 1)
+    }
+
+    setupHue()
+    {
+        this.hue = this.stage.addChild(new PIXI.Container())
+        this.hue.position.set(RADIUS, this.picker.y + this.picker.height + SPACING)
+        const hue = this.hue.addChild(this.sheet.get('hue'))
+        hue.anchor.set(0)
+        hue.height = BAR_HEIGHT
+        this.hue.interactive = true
+        this.hue.on('pointerdown', () => this.isHueDown = true)
+        this.hue.on('pointermove', (e) => this.hueMove(e))
+        this.hue.on('pointerup', () => this.isHueDown = false)
+        this.hue.on('pointerupoutside', () => this.isHueDown = false)
+        this.hueCursor = this.hue.addChild(this.sheet.get('bar'))
+        const color = new TinyColor('#' + State.color.substr(0, 6)).toHsl()
+        const x = color.h / 360 * this.content.offsetWidth - RADIUS * 2
+        this.hueCursor.position.set(x, BAR_HEIGHT / 2)
+    }
+
+    hueMove(e)
+    {
+        if (this.isHueDown)
+        {
+            const size = this.content.offsetWidth - RADIUS * 2
+            const local = this.picker.toLocal(e.data.global)
+            let x = local.x
+            this.hueCursor.x = x < 0 ? 0 : x > size ? size : x
+            const h = (x / size) * 360
+            const original = new TinyColor(State.color).toHsl()
+            const color = new TinyColor({ h: h, s: original.s, l: original.l }).toHex()
+            State.color = color + State.color.substr(6)
+            this.sheet.changeDraw('picker', (c) => this.drawPicker(c))
+            this.sheet.changeDraw('alpha', (c) => this.drawAlpha(c))
+            this.renderer.render(this.stage)
+        }
+    }
+
+    drawAlpha(c)
+    {
+        c.beginPath()
+        c.clearRect(0, 0, this.size(), BAR_HEIGHT)
+        const rgb = new TinyColor('#' + State.color.substr(0, 6)).toRgb()
+        let gradient = c.createLinearGradient(0, 0, this.size(), 0)
+        gradient.addColorStop(0, 'rgba(' + rgb.r + ',' + rgb.g + ',' + rgb.b + ',0)')
+        gradient.addColorStop(1, 'rgb(' + rgb.r + ',' + rgb.g + ',' + rgb.b + ')')
+        c.fillStyle = gradient
+        c.fillRect(0, 0, this.size(), BAR_HEIGHT)
     }
 
     setupAlpha()
     {
-        this.alpha = document.createElement('canvas')
-        this.alpha.style.width = '100%'
-        // this.alpha.addEventListener('mousedown', () => this.pickerDown())
-        // this.alpha.addEventListener('mousemove', (e) => this.alphaMove(e))
-        // this.ui.$overlay[0].addEventListener('mousemove', (e) => this.alphaMove(e))
-        // this.win.el[0].addEventListener('mousemove', (e) => this.alphaMove(e))
-        // this.alpha.addEventListener('mouseup', () => this.alphaUp())
-        // this.ui.$overlay[0].addEventListener('mouseup', () => this.alphaUp())
-        // this.win.el[0].addEventListener('mouseup', () => this.alphaUp())
-        this.content.appendChild(this.alpha)
-        this.drawAlpha()
+        const size = this.size()
+        this.alpha = this.stage.addChild(new PIXI.Container())
+        const transparent = this.alpha.addChild(new PIXI.extras.TilingSprite(sheet.getTexture('transparency')))
+        transparent.tileScale.set(0.1)
+        transparent.width = size
+        transparent.height = BAR_HEIGHT
+        this.alpha.position.set(RADIUS, this.hue.y + this.hue.height + SPACING)
+        const alpha = this.alpha.addChild(this.sheet.get('alpha'))
+        alpha.anchor.set(0)
+        alpha.height = BAR_HEIGHT
+        this.alpha.interactive = true
+        this.alpha.on('pointerdown', () => this.isAlphaDown = true)
+        this.alpha.on('pointermove', (e) => this.alphaMove(e))
+        this.alpha.on('pointerup', () => this.isAlphaDown = false)
+        this.alpha.on('pointerupoutside', () => this.isAlphaDown = false)
+        this.alphaCursor = this.alpha.addChild(this.sheet.get('bar'))
+        const x = size * parseInt(State.color.substr(6), 16) / 255
+        this.alphaCursor.position.set(x, BAR_HEIGHT / 2)
     }
 
-    drawAlpha()
+    alphaMove(e)
     {
-        const rgb = new TinyColor('#' + State.color.substr(0, 6)).toRgb()
-        this.alpha.width = this.content.offsetWidth
-        this.alpha.height = BAR_HEIGHT
-        const c = this.alpha.getContext('2d')
-        let gradient = c.createLinearGradient(0, 0, this.size, 0)
-        gradient.addColorStop(0, 'rgb(' + rgb.r + ',' + rgb.g + ',' + rgb.b + ')')
-        gradient.addColorStop(1, 'rgba(' + rgb.r + ',' + rgb.g + ',' + rgb.b + ',0)')
-        c.fillStyle = gradient
-        c.fillRect(RADIUS, RADIUS, this.size, BAR_HEIGHT)
+        if (this.isAlphaDown)
+        {
+            const size = this.content.offsetWidth - RADIUS * 2
+            const local = this.picker.toLocal(e.data.global)
+            let x = local.x
+            this.alphaCursor.x = x < 0 ? 0 : x > size ? size : x
+            let alpha = Math.floor((x / size) * 255)
+            alpha = (alpha > 255 ? 255 : alpha).toString(16)
+            alpha = alpha.length === 1 ? '0' + alpha : alpha
+            State.color = State.color.substr(0, 6) + alpha
+            this.renderer.render(this.stage)
+        }
     }
 
     wordsSetup()
@@ -502,9 +562,9 @@ console.log(x, y)
         this.win.el[0].addEventListener('touchmove', () => this.resized())
         this.win.el[0].addEventListener('mouseup', () => this.dragged())
         this.win.el[0].addEventListener('touchend', () => this.dragged())
-        State.on('foreground', this.draw, this)
-        State.on('background', this.draw, this)
-        State.on('isForeground', this.draw, this)
+        // State.on('foreground', this.draw, this)
+        // State.on('background', this.draw, this)
+        // State.on('isForeground', this.draw, this)
     }
 
     resized()
